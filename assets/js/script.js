32233dd92f49651b077e6d8bf04bf2018666fadc
David Paillacheo P <david.paillacheo@ticonsultores.cl>
2024-06-18 01:15:58 -0400

    index1-ready

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,11 @@ const onCl = document.querySelector("#ford")
 
 onCl.addEventListener("click", () => {
     const imgStyle = document.querySelector("#ford")
-    
-    imgStyle.style.border = "2px"
-    imgStyle.style.borderColor = "#ff0000"
+    imgStyle.classList.toggle("red-border")
 })
+
+
+
 
 
 
